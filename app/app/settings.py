bc58b4b14ae4fc64f5bd0f9f36a3b4802b51a061
Jitesh Shewale <jiteshshewale40@gmail.com>
2024-01-27 15:57:12 +0530

    sample test 7

--- app/app/settings.py
+++ app/app/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-x_ly%%nsa+@k#^ep@3n&9i00)kjf-xddrxd=x@0oh)t+(6k%r1'
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
     'django-insecure-x_ly%%nsa+@k#^ep@3n&9i00)kjf-xddrxd=x@0oh)t+(6k%r1'
 )
